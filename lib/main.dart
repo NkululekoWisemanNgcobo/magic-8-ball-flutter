e0d1c024ef752085bd2e0a4e1016c71837616550
Nkululeko <ngcobonw@gmail.com>
2021-04-10 13:37:59 +0200

    Magic 8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          body: Magic8(),
+        ),
       ),
     );
+
+class Magic8 extends StatefulWidget {
+  @override
+  _Magic8State createState() => _Magic8State();
+}
+
+class _Magic8State extends State<Magic8> {
+  int ballNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNum = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNum.png'),
+      ),
+    );
+  }
+}
